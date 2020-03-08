c4129a6676676df9c43264e24bd9403c0a2f8b07
Matt Shaw <buildlite@avant.com>
2020-03-07 20:35:24 -0500

    Lesson 2 - Class Example

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
+// I am a class - high notes, I can define child functions (render is the key here)
+// and I can use 'this' notation from standard javascript.
+export default class App {
+  public render () {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
